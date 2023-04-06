03b600b494a149169e1574e0e026ddcfd1dd1dd2
Terry <113734442+Terry-Yuxiang@users.noreply.github.com>
2023-04-05 23:18:46 -0700

    Initial Commit

---
+++ cs571Homework9/cs571Homework9App.swift
@@ -0,0 +1,17 @@
+//
+//  cs571Homework9App.swift
+//  cs571Homework9
+//
+//  Created by 樊宇祥 on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct cs571Homework9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
